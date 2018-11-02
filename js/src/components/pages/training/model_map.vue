be51752daaa4b2b9aba3c7223238c6465ce7f560
takuuuuumi34 <takuuuuumi.34@gmail.com>
2018-11-02 17:40:43 +0900

    custom d3 graph in training page

--- js/src/components/pages/training/model_map.vue
+++ js/src/components/pages/training/model_map.vue
@@ -5,15 +5,16 @@
         Model Map
       </div>
 
-      <div id="value-map" class="panel-content"></div>
+      <div id="value-map" class="panel-content">
+        <div class="x-axis-name">Root Mean Squared Error</div>
+        <div class="y-axis-name">Max Absolute Error</div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import * as d3 from 'd3'
-const width = 300
-const height = 300
 
 export default {
   name: 'ModelMap',
@@ -34,6 +35,11 @@ export default {
 
       const [rmse_list, max_abs_error_list, dataset] = this.shapeDataset()
 
+      const parent_area = d3.select('#value-map')
+      const width = parent_area._groups[0][0].clientWidth
+      const height = parent_area._groups[0][0].clientHeight
+      const margin = { 'left': 60, 'top': 40, 'right': 40, 'bottom': 60 }
+
       const svg = d3.select('#value-map')
         .append('svg')
         .attr('width', width)
@@ -41,31 +47,31 @@ export default {
 
       const xScale = d3.scaleLinear()
         .domain([0, d3.max(rmse_list, function (d) { return d })])
-        .range([0, width])
+        .range([margin.left, width - margin.right])
       const yScale = d3.scaleLinear()
         .domain([0, d3.max(max_abs_error_list, function (d) { return d })])
-        .range([height, 0])
+        .range([height - margin.bottom, margin.top])
 
       // get axes
       const axisx = d3.axisBottom(xScale)
-        .tickSizeInner(-height)
+        .tickSizeInner(-(height - margin.top - margin.bottom))
         .tickSizeOuter(0)
         .ticks(10)
         .tickPadding(10)
       const axisy = d3.axisLeft(yScale)
         .ticks(10)
-        .tickSizeInner(-width)
+        .tickSizeInner(-(width - margin.left - margin.right))
         .tickSizeOuter(0)
         .ticks(10)
         .tickPadding(10)
 
       // draw x axis
       let gX = svg.append('g')
-        .attr('transform', 'translate(' + 0 + ',' + height + ')')
+        .attr('transform', 'translate(' + 0 + ',' + (height - margin.bottom) + ')')
         .call(axisx)
       // draw y axis
       let gY = svg.append('g')
-        .attr('transform', 'translate(' + 0 + ',' + 0 + ')')
+        .attr('transform', 'translate(' + margin.left + ',' + 0 + ')')
         .call(axisy)
 
       stylingAxes()
@@ -101,7 +107,7 @@ export default {
       }
     },
     removeMap: function () {
-      d3.select('#value-map').selectAll('*').remove()
+      d3.select('#value-map').selectAll('svg').remove()
     },
     shapeDataset: function () {
       let rmse_list = []
@@ -121,5 +127,26 @@ export default {
 
 <style lang="scss" scoped>
 #model-map {
+  width: 100%;
+  height: $model-map-height;
+
+  #value-map {
+    position: relative;
+
+    .x-axis-name, .y-axis-name {
+      position: absolute;
+      font-size: 0.8rem;
+      color: $gray;
+    }
+    .x-axis-name {
+      bottom: 24px;
+      left: 50%;
+      @include prefix("transform", "translateX(-50%)");
+    }
+    .y-axis-name {
+      top: 16px;
+      left: 16px;
+    }
+  }
 }
 </style>
